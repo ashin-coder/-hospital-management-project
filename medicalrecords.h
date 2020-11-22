747be8249995b834ea908c010d3150ce99650f83
ashin-coder <73836674+ashin-coder@users.noreply.github.com>
2020-11-23 00:51:54 +0530

    Update medicalrecords.h

--- medicalrecords.h
+++ medicalrecords.h
@@ -1,1 +1,11 @@
 
+
+        struct medicalrecords *next; /*next is used to navigate through structures.*/
+    int count;     /*count is used to input comments into array*/
+    } medicalrecords;
+medicalrecords *firstc,*currentc,*newc; /*pointers*/
+/* firstc is used to point to first record in list
+currentc points to current record in list
+newc contains address of new structure/node
+*/
+int cnum = 0; /*gives unique account numbers*/
